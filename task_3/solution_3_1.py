a5de90532fe4b9734a7190253732429d9b95036f
entrapolarity <72064593+entrapolarity@users.noreply.github.com>
2020-10-24 01:38:44 +0300

    Update solution_3_1.py

--- task_3/solution_3_1.py
+++ task_3/solution_3_1.py
@@ -2,8 +2,6 @@
 user_input = input()
 alphabet = ""
 for char in user_input:
-    if char in alphabet:
-        alphabet = alphabet + ""
-    else:
+    if (char in alphabet) == False:
         alphabet = alphabet + char
 print(alphabet)
