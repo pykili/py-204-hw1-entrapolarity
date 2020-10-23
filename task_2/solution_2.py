f3fbbfc52ab1492a176cc58dea914668665cae06
entrapolarity <72064593+entrapolarity@users.noreply.github.com>
2020-10-24 00:54:31 +0300

    Update solution_2.py

--- task_2/solution_2.py
+++ task_2/solution_2.py
@@ -1,4 +1,9 @@
 # your code here
 user_input = input()
-# your code here
+most_frequent_character = ""
+max = 0
+for char in user_input:
+  if user_input.count(char) > max:
+    max = user_input.count(char)
+    most_frequent_character = char
 print(most_frequent_character)
