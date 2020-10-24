f9ae4eeec84b5edb162f987d39908cdc4b965e3e
entrapolarity <72064593+entrapolarity@users.noreply.github.com>
2020-10-24 12:43:42 +0300

    Update solution_8.py

--- task_8/solution_8.py
+++ task_8/solution_8.py
@@ -1,9 +1,9 @@
 # your code here
 n = int(input())
 num = 1
-count = 0
+count = -1 #не считаем 0 элементом последовательности
 sum = 0.0
-for smth in "a"*n:
+for smth in "a"*(n + 1): #могут вводиться N чисел, а потом 0
     if (num == 0) == False:
         num = int(input())
         sum = sum + num
