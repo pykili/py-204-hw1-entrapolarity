d9f977c7653daded4c735a3fa6c9379e90b4de4d
entrapolarity <72064593+entrapolarity@users.noreply.github.com>
2020-10-24 14:04:56 +0300

    Update solution_4_2.py

--- task_4/solution_4_2.py
+++ task_4/solution_4_2.py
@@ -1,6 +1,31 @@
 # your code here
-for smth in 'a'*10:
-    user_input = input()
-    # your code here
-    if my_cool_condition == True:
-        print(form, lemma)
+for smth in "a"*10:
+    str = input()
+    form = ""
+    lemma = ""
+    if (str == "") == False:
+        if (str[0] == "#") == False:
+            number_tabs = 0
+            index = 0
+            for smth in "a"*len(str):
+                current_char = str[index]
+                if current_char == "\t":
+                    number_tabs = number_tabs + 1
+                elif number_tabs == 1:
+                    form = form + current_char
+                elif number_tabs == 2:
+                    lemma = lemma + current_char
+                index = index + 1   
+            if (form == lemma) == False:
+                begins = False
+                if len(lemma) < len(form):
+                    index = 0
+                    count = 0
+                    for smth in "a"*len(lemma):
+                        if lemma[index] == form[index]:
+                            count = count + 1
+                        index = index + 1    
+                    if count == len(lemma):
+                        begins = True
+                if (begins == False):        
+                    print(form, lemma)
